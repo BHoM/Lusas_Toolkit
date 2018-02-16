385a71392abb50f33e0fe374afc0fc183e718360
pnugent <Peter.Nugent@burohappold.com>
2018-02-16 10:26:54 +0000

    Name change
    Lusas Adapter to Lusas_Legacy

--- Lusas_Adapter/My Project/Resources.Designer.vb
+++ Lusas_Adapter/My Project/Resources.Designer.vb
@@ -39,7 +39,7 @@ Namespace My.Resources
         Friend ReadOnly Property ResourceManager() As Global.System.Resources.ResourceManager
             Get
                 If Object.ReferenceEquals(resourceMan, Nothing) Then
-                    Dim temp As Global.System.Resources.ResourceManager = New Global.System.Resources.ResourceManager("Lusas_Adapter.Resources", GetType(Resources).Assembly)
+                    Dim temp As Global.System.Resources.ResourceManager = New Global.System.Resources.ResourceManager("Lusas_Legacy.Resources", GetType(Resources).Assembly)
                     resourceMan = temp
                 End If
                 Return resourceMan
