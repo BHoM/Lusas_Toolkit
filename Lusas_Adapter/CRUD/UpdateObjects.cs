553e3ab3f38ea91a17b5bff28e87128b4334411b
Chris Self <Chris.Self@BuroHappold.com>
2018-05-18 09:01:17 +0100

    Created update function for points

--- Lusas_Adapter/CRUD/UpdateObjects.cs
+++ Lusas_Adapter/CRUD/UpdateObjects.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BH.oM.Structural.Elements;
+using BH.oM.Geometry;
+using BH.oM.Structural.Properties;
+using BH.oM.Structural.Loads;
+using BH.oM.Common.Materials;
+using LusasM15_2;
 
 namespace BH.Adapter.Lusas
 {
@@ -18,6 +24,27 @@ namespace BH.Adapter.Lusas
         protected override bool UpdateObjects<T>(IEnumerable<T> objects)
         {
             return base.UpdateObjects<T>(objects);
+        }
+
+        protected bool Update(IEnumerable<Node> nodes)
+        {
+
+            foreach (Node node in nodes)
+            {
+                IFPoint LusasNode = d_LusasData.getPointByName(node.CustomData[AdapterId].ToString());
+
+                if (LusasNode == null)
+                {
+                    return false;
+                }
+                    
+                if (!string.IsNullOrWhiteSpace(node.Constraint.Name))
+                {
+                    d_LusasData.Delete(LusasNode);
+                    createpoint(node);
+                }
+            }
+            return true;
         }
 
         /***************************************************/
