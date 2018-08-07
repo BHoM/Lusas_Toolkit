8e458efba752b9ad4f20140ef4406989705ce5c4
Chris Self <Chris.Self@BuroHappold.com>
2018-08-07 18:14:33 +0100

    Fix Line Bug + section properties Read started

--- Lusas_Adapter/Types/DependencyTypes.cs
+++ Lusas_Adapter/Types/DependencyTypes.cs
@@ -46,8 +46,8 @@ namespace BH.Adapter.Lusas
         private static Dictionary<Type, List<Type>> m_DependencyTypes = new Dictionary<Type, List<Type>>
         {
             // {typeof(PanelPlanar), new List<Type> {typeof(Bar), typeof(Node) } },
-            {typeof(Bar), new List<Type> { typeof(Node) } },
-            {typeof(ISectionProperty), new List<Type> { typeof(Material) } },
+            {typeof(Bar), new List<Type> { typeof(Node), typeof(ISectionProperty) } },
+            //{typeof(ISectionProperty), new List<Type> { typeof(Material) } },
             {typeof(RigidLink), new List<Type> { typeof(LinkConstraint), typeof(Node) } },
             {typeof(MeshFace), new List<Type> { typeof(IProperty2D), typeof(Node) } },
             {typeof(IProperty2D), new List<Type> { typeof(Material) } },
