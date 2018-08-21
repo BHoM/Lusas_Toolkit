6c9cb048ae3069d4ac0193fb94ac8780312203d3
pnugent <Peter.Nugent@burohappold.com>
2018-08-21 14:56:08 +0100

    Structural namespace change
    Change of BH.oM.Structural to BH.oM.Structure namespace.

--- Lusas_Engine/Convert/ToBHoM/Edge.cs
+++ Lusas_Engine/Convert/ToBHoM/Edge.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BH.oM.Structural.Elements;
+using BH.oM.Structure.Elements;
 using BH.oM.Geometry;
 using BH.Engine.Geometry;
 using Lusas.LPI;
