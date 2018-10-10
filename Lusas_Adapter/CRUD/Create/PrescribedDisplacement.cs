2583fa9c96f5c43e2eb1eb77f26d7597cbe08cde
pnugent <Peter.Nugent@burohappold.com>
2018-10-10 18:57:39 +0100

    Read Write Prescribed Displacements

---
+++ Lusas_Adapter/CRUD/Create/PrescribedDisplacement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using BH.oM.Base;
+using BH.oM.Structure.Elements;
+using BH.oM.Geometry;
+using BH.oM.Structure.Properties;
+using BH.oM.Structure.Loads;
+using BH.oM.Common.Materials;
+using BH.Engine.Geometry;
+using BH.Engine.Structure;
+using Lusas.LPI;
+
+namespace BH.Adapter.Lusas
+{
+    public partial class LusasAdapter
+    {
+        public IFPrescribedDisplacementLoad CreatePrescribedDisplacement(PointDisplacement pointDisplacement, IFPoint[] lusasPoints)
+        {
+
+            IFPrescribedDisplacementLoad lusasPrescribedDisplacement = null;
+            IFLoadcase assignedLoadcase = (IFLoadcase)d_LusasData.getLoadset("Lc" + pointDisplacement.Loadcase.CustomData[AdapterId] + "/" + pointDisplacement.Loadcase.Name);
+            string lusasAttributeName = "Pd" + pointDisplacement.CustomData[AdapterId] + "/" + pointDisplacement.Name;
+
+            if (d_LusasData.existsAttribute("Loading", lusasAttributeName))
+            {
+                lusasPrescribedDisplacement = (IFPrescribedDisplacementLoad)d_LusasData.getAttribute("Loading", lusasAttributeName);
+            }
+            else
+            {
+                List<string> valueNames = new List<string> { "u", "v", "w", "thx", "thy", "thz" };
+                List<string> boolCheck = new List<string> { "haveDispX", "haveDispY", "haveDispZ",
+                    "haveRotX", "haveRotY", "haveRotZ" };
+                List<double> displacements = new List<double>
+                {
+                    pointDisplacement.Translation.X, pointDisplacement.Translation.Y, pointDisplacement.Translation.Z,
+                    pointDisplacement.Rotation.X,pointDisplacement.Rotation.Y, pointDisplacement.Rotation.Z
+                };
+
+                lusasPrescribedDisplacement = d_LusasData.createPrescribedDisplacementLoad(lusasAttributeName, "Total");
+
+                for(int i=0; i < valueNames.Count(); i++)
+                {
+                    if(!(displacements[i] == 0))
+                    {
+                        lusasPrescribedDisplacement.setValue(boolCheck[i], true);
+                        lusasPrescribedDisplacement.setValue(valueNames[i], displacements[i]);
+                    }
+                    else
+                    {
+                        lusasPrescribedDisplacement.setValue(boolCheck[i], false);
+                    }
+                }
+
+            }
+
+            IFAssignment assignToNodes = m_LusasApplication.assignment();
+            assignToNodes.setLoadset(assignedLoadcase);
+            lusasPrescribedDisplacement.assignTo(lusasPoints, assignToNodes);
+
+            return lusasPrescribedDisplacement;
+        }
+    }
+}
---
+++ Lusas_Engine/Convert/ToBHoM/PointDisplacement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Base;
+using BH.oM.Geometry;
+using BH.oM.Structure.Elements;
+using BH.oM.Structure.Properties;
+using BH.oM.Structure.Loads;
+using BH.oM.Common.Materials;
+using BH.Engine.Lusas;
+using Lusas.LPI;
+
+namespace BH.Engine.Lusas
+{
+    public static partial class Convert
+    {
+        public static PointDisplacement ToPointDisplacement(IFLoading lusasPrescribedDisplacement, IEnumerable<IFAssignment> assignmentList, Dictionary<string, Node> nodes)
+        {
+            IFLoadcase assignedLoadcase = (IFLoadcase)assignmentList.First().getAssignmentLoadset();
+            Loadcase bhomLoadcase = BH.Engine.Lusas.Convert.ToBHoMLoadcase(assignedLoadcase);
+
+            IEnumerable<Node> bhomNodes = GetNodeAssignments(assignmentList, nodes);
+
+            lusasPrescribedDisplacement.getValueNames();
+
+            Vector translationVector = new Vector { X = lusasPrescribedDisplacement.getValue("U"), Y = lusasPrescribedDisplacement.getValue("V"), Z = lusasPrescribedDisplacement.getValue("W") };
+            Vector rotationVector = new Vector { X = lusasPrescribedDisplacement.getValue("THX"), Y = lusasPrescribedDisplacement.getValue("THY"), Z = lusasPrescribedDisplacement.getValue("THZ") };
+
+            PointDisplacement bhomPointDisplacement = BH.Engine.Structure.Create.PointDisplacement(bhomLoadcase, bhomNodes, translationVector, rotationVector, LoadAxis.Global, GetName(lusasPrescribedDisplacement));
+
+            int bhomID = GetBHoMID(lusasPrescribedDisplacement, 'd');
+            bhomPointDisplacement.CustomData["Lusas_id"] = bhomID;
+            return bhomPointDisplacement;
+
+            // Needs to be a bit here that determines whether it is global or local - actually this cannot be done as the 
+            //attribute is applied to a group, and within the group the axis could local or global
+
+        }
+    }
+}
