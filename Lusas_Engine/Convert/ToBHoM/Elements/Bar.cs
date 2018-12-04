49160addda354c39bba30a54a44ebafe243b669a
Chris Self <Chris.Self@BuroHappold.com>
2018-12-04 15:25:24 +0000

    Bars can now have MeshSettings1D put in as CustomData to assign mesh in Lusas

--- Lusas_Engine/Convert/ToBHoM/Elements/Bar.cs
+++ Lusas_Engine/Convert/ToBHoM/Elements/Bar.cs
@@ -63,7 +63,7 @@ namespace BH.Engine.Lusas
             if (!(meshAssignments.Count()==0))
             {
                 bhomMeshes.TryGetValue(meshAssignments[0], out lineMesh);
-                bhomBar.CustomData["Mesh"] = lineMesh.Name;
+                bhomBar.CustomData["Mesh"] = "Me" + lineMesh.CustomData["Lusas_id"] + "/" + lineMesh.Name;
             }
 
             string adapterID = RemovePrefix(lusasLine.getName(), "L");
