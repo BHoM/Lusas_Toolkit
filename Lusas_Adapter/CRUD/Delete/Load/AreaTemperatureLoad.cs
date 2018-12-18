b21ccac0d4d297159b6ce605ad7f2b592d156ab4
pnugent <Peter.Nugent@burohappold.com>
2018-12-18 18:42:44 +0000

    Delete loads
    Some delete methods for loads, not complete

---
+++ Lusas_Adapter/CRUD/Delete/Load/AreaTemperatureLoad.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace BH.Adapter.Lusas
+{
+    public partial class LusasAdapter
+    {
+        public int DeleteAreaTemperatureLoad(IEnumerable<object> ids)
+        {
+            int success = 1;
+
+            if (ids != null)
+            {
+                Engine.Reflection.Compute.RecordError("The deleting of individual AreaTemperatureLoad objects is not supported in the Lusas_Toolkit");
+
+                return 0;
+            }
+            else
+            {
+                object[] lusasTemperatureLoads = d_LusasData.getAttributes("Temperature");
+                DeleteSurfaceAssignments(lusasTemperatureLoads);
+            }
+            return success;
+        }
+    }
+}
---
+++ Lusas_Adapter/CRUD/Delete/Load/AreaUniformlyDistributed.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BH.Adapter.Lusas
+{
+    public partial class LusasAdapter
+    {
+        public int DeleteAreaUnformlyDistributedLoads(IEnumerable<object> ids)
+        {
+            int success = 1;
+
+            if (ids != null)
+            {
+                Engine.Reflection.Compute.RecordError("The deleting of individual AreaUmiformlyDistributedLoad objects is not supported in the Lusas_Toolkit");
+
+                return 0;
+            }
+            else
+            {
+                object[] lusasGlobalDistributedLoads = d_LusasData.getAttributes("Global Distributed Load");
+                object[] lusasLocalDistributedLoads = d_LusasData.getAttributes("Distributed Load");
+
+                object[] lusasDistributedLoads = lusasGlobalDistributedLoads.Concat(
+                    lusasLocalDistributedLoads).ToArray();
+
+                DeleteSurfaceAssignments(lusasDistributedLoads);
+            }
+            return success;
+        }
+    }
+}
