0bf15ba0cbca3ca44a267daf17678c3ed796e3a3
pnugent <Peter.Nugent@burohappold.com>
2018-12-18 15:32:25 +0000

    Delete methods
    Add delete methods for geometry

---
+++ Lusas_Adapter/CRUD/Delete/_Delete.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using BH.oM.Structure.Elements;
+using BH.oM.Structure.Properties.Section;
+
+namespace BH.Adapter.Lusas
+{
+    public partial class LusasAdapter
+    {
+        protected override int Delete(Type type, IEnumerable<object> ids)
+        {
+            int success = 0;
+
+            if (type == typeof(Node))
+                success = DeletePoints(ids);
+            if (type == typeof(Bar))
+                success = DeleteLines(ids);
+            if (type == typeof(PanelPlanar))
+                success = DeleteSurfaces(ids);
+            //if (type == typeof(ISectionProperty))
+            //    success = DeleteSectionProperties(ids);
+
+            return 0;
+        }
+    }
+}
--- Lusas_Adapter/CRUD/Read/All.cs
+++ Lusas_Adapter/CRUD/Read/All.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using BH.oM.Base;
-using BH.oM.Geometry;
-using BH.oM.Structure.Elements;
-using BH.oM.Structure.Properties;
-using BH.oM.Structure.Loads;
-using BH.oM.Common.Materials;
-using Lusas.LPI;
-using BH.oM.Adapters.Lusas;
 
 namespace BH.Adapter.Lusas
 {
