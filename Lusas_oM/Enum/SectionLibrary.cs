714a0c0864ea401a7c034facec076743a4aaefaa
peterjamesnugent <Peter.Nugent@burohappold.com>
2020-02-21 12:04:56 +0000

    Fixes compliance issue for HasSingleClass

--- Lusas_oM/Enum/SectionLibrary.cs
+++ Lusas_oM/Enum/SectionLibrary.cs
@@ -22,19 +22,16 @@
 
 namespace BH.oM.Adapters.Lusas
 {
-    public enum Split1D
-    {
-        Divisions, Length, Automatic
-    }
-
-    public enum Split2D
-    {
-        Divisions, Size, Automatic
-    }
+    /***************************************************/
+    /**** Public Enums                              ****/
+    /***************************************************/
 
     public enum SectionLibrary
     {
         UK_Sections, EU_Sections
     }
+
+    /***************************************************/
+
 }
 
