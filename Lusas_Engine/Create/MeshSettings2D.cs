8874231e3363d07c875af3039d9e3dc6f1c44230
peterjamesnugent <Peter.Nugent@burohappold.com>
2020-02-21 12:05:23 +0000

    Fixes compliance issue for CreateMethodFileNameMatchesReturnType

--- Lusas_Engine/Create/MeshSettings2D.cs
+++ Lusas_Engine/Create/MeshSettings2D.cs
@@ -31,18 +31,6 @@ namespace BH.Engine.Adapters.Lusas
         /****           Public Constructors             ****/
         /***************************************************/
 
-        public static MeshSettings1D MeshSettings1D(Split1D SplitMethod,
-            double SplitParameter = 4, string name = null)
-        {
-            return new MeshSettings1D
-            {
-                SplitMethod = SplitMethod,
-                SplitParameter = SplitParameter,
-                Name = name
-            };
-        }
-
-
         public static MeshSettings2D MeshSettings2D(Split2D splitMethod,
             int xDivisions = 0, int yDivisions = 0, double size = 1, string name = null)
         {
@@ -55,6 +43,9 @@ namespace BH.Engine.Adapters.Lusas
                 Name = name
             };
         }
+
+        /***************************************************/
+
     }
 }
 
