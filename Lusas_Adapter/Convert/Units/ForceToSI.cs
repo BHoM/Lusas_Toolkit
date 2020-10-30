c50f2a927a9b1a0effee63d2fd83b68aefbcded9
StephennipBH <Stephen.nip@Burohappold.com>
2020-10-30 11:10:40 +0000

    Adding convert methods

--- Lusas_Adapter/Convert/Units/ForceToSI.cs
+++ Lusas_Adapter/Convert/Units/ForceToSI.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using BH.Adapter.Lusas;
+using BH.oM.Geometry;
+using BH.Engine.Reflection;
+using BH.oM.Structure.Constraints;
+using BH.oM.Structure.Elements;
+using BH.Engine.Units;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace BH.Adapter.Lusas.Convert.Units
+namespace BH.Adapter.Adapters.Lusas
 {
-    class ForceToSI
+    public static partial class Convert
     {
+        public static double ForceToSI(this double force, string forceUnit)
+        {
+            switch (forceUnit)
+            {
+                case "N":
+                    break;
+                case "KN":
+                    return force.FromKilonewton();
+                case "KGF":
+                    return force.FromKilogramForce();
+                case "TONF":
+                    return force.FromTonneForce();
+                case "LBF":
+                    return force.FromPoundForce();
+                case "KIPS":
+                    return force.FromKilopoundForce();
+                default:
+                    Compute.RecordWarning("No firce unit detected, Lusas force unit assumed to be set to metres. Therefore no unit conversion will occur. ");
+                    break;
+            }
+
+            return force;
+        }
+
+        /***************************************************/
+
     }
 }
