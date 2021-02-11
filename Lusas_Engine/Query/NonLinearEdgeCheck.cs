43fc11c21ee9ac91bbd88aaea667983ff8e4b475
Peter Nugent <Peter.Nugent@burohappold.com>
2021-02-11 19:37:57 +0000

    Fixes for code-compliance for extension method

--- Lusas_Engine/Query/NonLinearEdgeCheck.cs
+++ Lusas_Engine/Query/NonLinearEdgeCheck.cs
@@ -33,7 +33,7 @@ namespace BH.Engine.Adapters.Lusas
         /***************************************************/
         /**** Private Methods                           ****/
         /***************************************************/
-        public static bool InvalidEdgeCheck(Edge edge)
+        public static bool InvalidEdgeCheck(this Edge edge)
         {
             bool isInvalid = true;
 
