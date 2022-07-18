db753a4018c2b3b320a4d64efd30cd258815e07a
FraserGreenroyd <fraser.greenroyd@burohappold.com>
2022-07-18 17:38:04 +0100

    Update Assembly Information

--- Lusas_oM/Properties/AssemblyInfo.cs
+++ Lusas_oM/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Lusas_oM")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/BHoM/Lusas_Toolkit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Lusas_oM")]
