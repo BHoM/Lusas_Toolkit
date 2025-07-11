3b5b8faed5f0051616c1b9f45a86b0c38ba3f899
Emma Sander <emma.sander@burohappold.com>
2025-07-11 16:07:58 +0100

    Added References

--- Lusas_Adapter/CRUD/Read/Results/ReadResults.cs
+++ Lusas_Adapter/CRUD/Read/Results/ReadResults.cs
@@ -48,6 +48,8 @@ namespace BH.Adapter.Lusas
     public partial class LusasV210Adapter
 #elif Debug211 || Release211
     public partial class LusasV211Adapter
+#elif Debug220 || Release220
+    public partial class LusasV220Adapter
 #else
     public partial class LusasV17Adapter : BHoMAdapter
 #endif
